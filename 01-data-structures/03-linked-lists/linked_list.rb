0cd851bb53d4386b139cd64d648ee065720aabc4
Alex Rolfe <rolfepercussion@gmail.com>
2016-07-15 08:31:18 -0500

    Added method advance_through_list_until_next_is to remove repeated code

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -4,13 +4,6 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
-  def advance_through_list_until_next_is(target)
-    @current_node = @head
-    until @current_node.next == target
-      @current_node = @current_node.next
-    end
-  end
-
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
     if @head.nil?
@@ -27,8 +20,7 @@ class LinkedList
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
-
-    if current_node == @tail
+    if @head == @tail
       @tail = nil
       @head = nil
       return nil
@@ -36,14 +28,15 @@ class LinkedList
 
     advance_through_list_until_next_is(@tail)
 
-    @tail = current_node
+    @tail = @current_node
     @tail.next = nil
   end
 
   # This method prints out a representation of the list.
   def print
     current_node = @head
-
+    # if I rework this so the puts is elsewhere,
+    # I can replace with the method advance_through_list_until_next_is(nil)
     until current_node.next.nil?
       puts current_node.data
       current_node = current_node.next
@@ -60,23 +53,16 @@ class LinkedList
       @head = temp_node
       return
     end
-
-    current_node = @head
-
     # removes tail
     if node == @tail
-      until current_node.next == @tail
-        current_node = current_node.next
-      end
-      current_node.next = nil
-      @tail = current_node
+      advance_through_list_until_next_is(@tail)
+      @current_node.next = nil
+      @tail = @current_node
       return
     end
     # removes from middle
-    until current_node.next == node
-      current_node = current_node.next
-    end
-    current_node.next = node.next
+    advance_through_list_until_next_is(node)
+    @current_node.next = node.next
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
@@ -103,6 +89,13 @@ class LinkedList
       @head.next = nil
       @head = new_head
       removed_head
+    end
+  end
+
+  def advance_through_list_until_next_is(target)
+    @current_node = @head
+    until @current_node.next == target
+      @current_node = @current_node.next
     end
   end
 end
--- 01-data-structures/03-linked-lists/linked_list_spec.rb
+++ 01-data-structures/03-linked-lists/linked_list_spec.rb
@@ -18,65 +18,65 @@ RSpec.describe LinkedList, type: Class do
     end
   end
 
-  # describe "#remove_tail" do
-  #   it "removes nodes from the tail" do
-  #     llist.add_to_tail(n1)
-  #     llist.add_to_tail(n2)
-  #     expect(llist.tail).to eq n2
-  #     llist.remove_tail
-  #     expect(llist.tail).to eq n1
-  #     llist.remove_tail
-  #     expect(llist.tail).to eq nil
-  #   end
-  # end
-  #
-  # describe "#print" do
-  #   before do
-  #     llist.add_to_tail(n1)
-  #     llist.add_to_tail(n2)
-  #   end
-  #
-  #   specify { expect { llist.print }.to output("Rob\nBen\n").to_stdout }
-  # end
-  #
-  # describe "#delete" do
-  #   before do
-  #     llist.add_to_tail(n1)
-  #     llist.add_to_tail(n2)
-  #     llist.add_to_tail(n3)
-  #   end
-  #
-  #   it "removes the head of a list properly" do
-  #     llist.delete(n1)
-  #     expect(llist.head).to eq n2
-  #     expect(llist.head.next).to eq n3
-  #     expect(llist.tail).to eq n3
-  #   end
-  #
-  #   it "removes the middle element of a list properly" do
-  #     llist.delete(n2)
-  #     expect(llist.head).to eq n1
-  #     expect(llist.head.next).to eq n3
-  #     expect(llist.tail).to eq n3
-  #   end
-  #
-  #   it "removes the last element of a list properly" do
-  #     llist.delete(n3)
-  #     expect(llist.head).to eq n1
-  #     expect(llist.head.next).to eq n2
-  #     expect(llist.tail).to eq n2
-  #   end
-  # end
-  #
-  # describe "#add_to_front" do
-  #   it "adds the node to the front of the linked list" do
-  #     llist.add_to_front(n1)
-  #     expect(llist.head).to eq n1
-  #     llist.add_to_front(n2)
-  #     expect(llist.head).to eq n2
-  #   end
-  # end
-  #
+  describe "#remove_tail" do
+    it "removes nodes from the tail" do
+      llist.add_to_tail(n1)
+      llist.add_to_tail(n2)
+      expect(llist.tail).to eq n2
+      llist.remove_tail
+      expect(llist.tail).to eq n1
+      llist.remove_tail
+      expect(llist.tail).to eq nil
+    end
+  end
+
+  describe "#print" do
+    before do
+      llist.add_to_tail(n1)
+      llist.add_to_tail(n2)
+    end
+
+    specify { expect { llist.print }.to output("Rob\nBen\n").to_stdout }
+  end
+
+  describe "#delete" do
+    before do
+      llist.add_to_tail(n1)
+      llist.add_to_tail(n2)
+      llist.add_to_tail(n3)
+    end
+
+    it "removes the head of a list properly" do
+      llist.delete(n1)
+      expect(llist.head).to eq n2
+      expect(llist.head.next).to eq n3
+      expect(llist.tail).to eq n3
+    end
+
+    it "removes the middle element of a list properly" do
+      llist.delete(n2)
+      expect(llist.head).to eq n1
+      expect(llist.head.next).to eq n3
+      expect(llist.tail).to eq n3
+    end
+
+    it "removes the last element of a list properly" do
+      llist.delete(n3)
+      expect(llist.head).to eq n1
+      expect(llist.head.next).to eq n2
+      expect(llist.tail).to eq n2
+    end
+  end
+
+  describe "#add_to_front" do
+    it "adds the node to the front of the linked list" do
+      llist.add_to_front(n1)
+      expect(llist.head).to eq n1
+      llist.add_to_front(n2)
+      expect(llist.head).to eq n2
+    end
+  end
+  
   # describe "#remove_front" do
   #   it "removes the node to the front of the linked list" do
   #     llist.add_to_front(n1)
