d246dafc782c8451d13d8c4ef7389c288a45b6fe
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-02 09:56:43 -0500

    Adds psuedocode frame from assignment text

---
+++ algorithms/7-np-complexity/tsp_greed.rb
@@ -0,0 +1,14 @@
+def nearest_possible_neighbor(cities_graph, current_city)
+  unless current_city.visited == false
+    neighbor_cities == current_city.neighbors
+    next_city = neighbor_cities[0]
+
+    for current_neighbor in neighbor_cities
+      if current_neighbor.distance < next_city.distance
+        next_city = current_neighbor
+      end
+    end
+    current_city.visited = true
+    current_city = next_city
+  end
+end
