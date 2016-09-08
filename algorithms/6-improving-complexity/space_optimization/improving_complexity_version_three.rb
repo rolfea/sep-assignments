92ee4a33d6cda2e49f046bc78c7603f332495064
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-08 14:52:50 -0500

    Adds get_middle_node method to find mid point using fast/slow pointer method. Edits sort_list method to use this helper method in implementing merge sort on linked list

--- algorithms/6-improving-complexity/space_optimization/improving_complexity_version_three.rb
+++ algorithms/6-improving-complexity/space_optimization/improving_complexity_version_three.rb
@@ -1,29 +1,26 @@
-require_relative 'linked_list'
+require_relative 'node.rb'
 
 # is passing the whole object defeating the space benefits of the linked list?
 def sort_list(head_node)
   # check is list is empty or a single item
   return head_node if head_node.nil? || head_node.next.nil?
   # find the middle node and split the list into two halves
-  middle_node_location = ((@length / 2).floor) - 1
-  middle_node = return_single(middle_node_location)
-  head_of_second_half = return_single(middle_node_location + 1)
+  middle_node = get_middle_node(head_node)
   # split the list
   middle_node.next = nil
-
   #merge the results of recursive calls to sort function (merge sort)
   merge(sort_list(head_node), sort_list(head_of_second_half)
 end
 
 def merge(list_1, list_2)
   merged_list = Node.new
-
+  # checks for single nodes
   if list_1.data.nil?
     list_2
   elsif list_2.data.nil?
     list_1
   end
-
+  # recursive sort of items in micro-lists
   if list_1.data <= list_2.data
     merged_list = list_1
     merged_list.next = merge(list_1.next, list_2)
@@ -32,4 +29,18 @@ def merge(list_1, list_2)
     merged_list.next = merge(list_1, list_2.next)
   end
   return merged_list
+end
+
+def get_middle_node(head_node)
+  slow_pointer = head_node
+  fast_pointer = head_node.next
+
+  if fast_pointer.data != nil
+    fast_pointer = fast_pointer.next
+  end
+  if fast_pointer != nil
+    fast_pointer = fast_pointer.next
+    slow_pointer = slow_pointer.next
+  end
+  slow_pointer
 end
