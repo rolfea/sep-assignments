5cef27649a4f44fcd7c582d7a4db14fca05cd22a
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-09 17:16:20 -0500

    Adds city and neighbor objects and a cities_visited hash to facilitate the neares_neighbor algorithm.

--- algorithms/7-np-complexity/tsp_greed.rb
+++ algorithms/7-np-complexity/tsp_greed.rb
@@ -1,39 +1,51 @@
+# cities and if they have been visited
+cities_visited = {A: false, B: false, C: false, D: false}
+# each city and it's neighbors, which should be an object with a distance property
+A.neighbors = [B = 2, C = 3, D = 11]
+B.neighbors = [A = 2, C = 4, D = 10]
+C.neighbors = [A = 3, B = 4, D = 9]
+D.neighbors = [A = 11, B = 10, C = 9]
+
 def nearest_possible_neighbor(cities_graph, current_city)
-  starting_city = current_city
+  all_cities_visited = false
 
-  while current_city.visited == false
+  while all_cities_visited == false
     neighbor_cities = current_city.neighbors
-    next_city = neighbor_cities[0]
-
-    for current_neighbor in neighbor_cities
-      if next_city.visited == true && current_neighbor.distance < next_city.distance
-        next_city = current_neighbor
+    next_city = nil
+    # searches for an unvisited neighbor city
+    for i in 0..neighbor_cities.length - 1
+      if cities_visited[:neighbor_cities[i]] == false
+        next_city = neighbor_cities[i]
+        break
       end
     end
-    current_city.visited = true
+    # if all neighbors are visited, you are finished
+    return if next_city == nil
+    # loops through the neighbors to find the smallest distance to an unvisited city
+    for i in 0..neighbor_cities.length - 1
+      if neighbor_cities[i].distance < next_city.distance && cities_visited[:neighbor_cities[i]] == false
+        next_city = neighbor_cities[i]
+      end
+    end
+    # sets the current city to visited
+    cities_visited[:current_city] = true
+    # sets the loop to the next city
     current_city = next_city
-  end
-end
-
-def nearest_possible_neighbor2(cities_graph, current_city)
-  neighbor_cities = current_city.neighbors
-  next_city = neighbor_cities[0]
-  for current_neighbor in neighbor_cities
-    if current_neighbor.distance < next_city.distance
-      next_city = current_neighbor
+    # checks if all cities have been visited
+    cities_visited.each_value do |value|
+      if value == true
+		    all_cities_visited = true
+	    else
+		    all_cities_visited = false
+		    break
+	    end
     end
   end
-  current_city.visited = true
-  current_city = next_city
-  if # all cities have been visited
-    return
-  else
-    nearest_possible_neighbor2(cities_graph, current_city)
-  end
 end
-
 # sample that highlights the bug
-city_a.neighbor_cities = {city_b: 1, city_c: 3}
-city_b.neighbor_cities = {city_c: 2, city_d: 4}
-city_c.neighbor_cities = {city_a: 3, city_d: 5}
-city_d.neighbor_cities = {city_b: 4, city_c: 5}
+ab = 2
+ac = 3
+ad = 11
+bc = 4
+bd = 10
+cd = 9
